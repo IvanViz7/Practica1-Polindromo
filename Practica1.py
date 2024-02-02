11a55309b34f1047838cd8b7cebf30fca086cf1a
Ivan Vizcarra <ivanvizcarra@MacBook-Air.local>
2024-02-02 02:49:09 -0600

    Se actualizan errores en el software

--- Practica1.py
+++ Practica1.py
@@ -1,16 +1,14 @@
 #Se le pide al usuario que ingrese una frase o serie de numeros
 frase_del_usuario = input('Dime una frase o una serie de números y yo te dire si es palindromo o no: ')
 
-#Se cuentan los caracteres alfanumericos que el usurario 
-cantidad_de_caracteres = len(''.join(filter(lambda char: char.isalnum(), frase_del_usuario)))
-print(cantidad_de_caracteres)
+#Se convierte la frase del usuario en un texto de solo letras mayúsculas, esto con la intensión de evitar confunfundir al software con la mezcla de mayúsculas y minisculas
+frase_del_usuario = frase_del_usuario.upper()
 
-#Los caracteres alfanumericos de frase_de_usuario se pasan a una lista
+#Se crea una lista y se eliminas los caracteres especiales y espacios para evitar confundir al programa (solo toma en cuenta caracteres alfanumericos)
 lista_de_caracteres = list(''.join(filter(lambda char: char.isalnum(), frase_del_usuario)))
-lista_reverse = lista_de_caracteres
-lista_reverse.reverse()
 
-if (lista_de_caracteres == lista_reverse):
+#Se realiza la comparación entre la lista y la misma lista inversa para así derterminar si es palindrome o no
+if (lista_de_caracteres == lista_de_caracteres[::-1]):
     print('La frase o lista de números es exactamente palindromo')
 else:
     print('La frase o lista de números no es palindromo')
